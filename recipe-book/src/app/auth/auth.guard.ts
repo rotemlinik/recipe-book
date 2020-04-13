e67b7811ac5b0ad0ef2962ec298fc488197f15b5
Rotem Linik <50616823+rotemlinik@users.noreply.github.com>
2020-04-13 11:34:09 +0300

    create auth guard for redirecting if not authenticated

---
+++ recipe-book/src/app/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): 
+    boolean | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
+      const isAuthenticated = user != null;
+      if (isAuthenticated) {
+        return true;
+      }
+
+      return this.router.createUrlTree(['/auth']);
+    }));
+  }
+}
